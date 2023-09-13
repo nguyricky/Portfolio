8edca825bb6ac2b77be2db71066a7cb65dbf3cd7
Ricky Nguyen <105029551+nguyricky@users.noreply.github.com>
2023-09-13 04:26:23 +0000

    Started work on the portfolio

--- src/components/pages/WIP.jsx
+++ src/components/pages/WIP.jsx
@@ -4,6 +4,8 @@ const WIP = () => {
     return (
         <section className="h-screen w-full relative mx-auto flex justify-center items-center bg-secondary">
           <div className="flex flex-col justify-center items-center">
+            <img className="w-1/4 h-1/4 animate-pulse"src={LogoImage}></img>
+            <h1 className="font-bold text-[35px] animate-pulse text-white mt-10">COMING SOON...</h1>
           </div>
         </section>
     )
